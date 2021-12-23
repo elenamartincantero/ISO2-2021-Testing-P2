d531f7b2f8aa2773cafaf00bef28965ca22a376c
Elena Martín Cantero <72609858+elenamartincantero@users.noreply.github.com>
2021-12-23 18:57:39 +0100

    Decision coverage and mcdc test cases

--- A04-Testing/Ex02/src/test/java/org/Ex02/AppTest.java
+++ A04-Testing/Ex02/src/test/java/org/Ex02/AppTest.java
@@ -1,4 +1,4 @@
-package org.Ex02;
+package Testing.A04;
 
 import static org.junit.Assert.assertEquals;
 
@@ -81,6 +81,7 @@ public class AppTest
 		}
     	
     }
+    @Test
     public void pairwiseRun() {
     	try {
 			assertEquals(5.0, (App.run(new Person(false, 38, false, true, false), 99)), 0.0);
@@ -111,5 +112,112 @@ public class AppTest
 			
 		}
     	
+    }
+    @Test
+    public void decisionlevel0() {
+    	assertEquals(2.0, (App.level0(new Person(false, 22, false, true, false))), 0.0);
+    	assertEquals(5.0, (App.level0(new Person(false, 24, false, true, false))), 0.0);
+    	assertEquals(1.0, (App.level0(new Person(false, 66, false, true, false))), 0.0);
+    	assertEquals(5.0, (App.level0(new Person(false, 64, false, true, false))), 0.0);
+    }
+    @Test
+    public void decisionlevel1() {
+    	assertEquals(3.5, (App.level1(new Person(false, 22, false, true, false))), 0.0);
+    	assertEquals(5.0, (App.level1(new Person(false, 24, false, true, false))), 0.0);
+    	assertEquals(2.5, (App.level1(new Person(false, 66, false, true, false))), 0.0);
+    	assertEquals(5.0, (App.level1(new Person(false, 64, false, true, false))), 0.0);
+    }
+    @Test
+    public void decisionlevel2() {
+    	assertEquals(6.0, (App.level2(new Person(false, 66, false, true, false))), 0.0);
+    	assertEquals(5.0, (App.level2(new Person(false, 64, false, true, false))), 0.0);
+    }
+    @Test
+    public void decisionlevel3() {
+    	assertEquals(6.0, (App.level3(new Person(false, 22, false, true, false))), 0.0);
+    	assertEquals(5.0, (App.level3(new Person(false, 24, false, true, false))), 0.0);
+    	assertEquals(7.5, (App.level3(new Person(false, 66, false, true, false))), 0.0);
+    	assertEquals(5.0, (App.level3(new Person(false, 64, false, true, false))), 0.0);
+    }
+    @Test
+    public void decisionlevel4() {
+    	assertEquals(7.5, (App.level4(new Person(false, 22, false, true, false))), 0.0);
+    	assertEquals(5.0, (App.level4(new Person(false, 24, false, true, false))), 0.0);
+    }
+    @Test
+    public void decisionValid() {
+    	assertEquals(true, (App.valid(new Person(false, 66, false, true, false), 499)));
+    	assertEquals(false, (App.valid(new Person(true, 64, true, false, true), 501)));
+    }
+    @Test
+    public void decisionRun() {
+    	try {
+			assertEquals(5.0, (App.run(new Person(false, 38, false, true, false), 99)), 0.0);
+			assertEquals(NotAllowedException.class, (App.run(new Person(true, 38, true, false, true), 101)));
+			assertEquals(5.0, (App.run(new Person(false, 38, false, true, false), 199)), 0.0);
+			assertEquals(NotAllowedException.class, (App.run(new Person(true, 38, true, false, true), 201)));
+			assertEquals(5.0, (App.run(new Person(false, 38, false, true, false), 300)), 0.0);
+			assertEquals(NotAllowedException.class, (App.run(new Person(true, 38, true, false, true), 301)));
+			assertEquals(5.0, (App.run(new Person(false, 38, false, true, false), 500)), 0.0);
+			assertEquals(NotAllowedException.class, (App.run(new Person(true, 38, true, false, true), 501)));
+			assertEquals(5.0, (App.run(new Person(false, 38, false, true, false), 501)), 0.0);
+			assertEquals(NotAllowedException.class, (App.run(new Person(true, 38, true, false, true), 499)));
+    	} catch (NotAllowedException e) {
+			
+		}
+    	
+    }
+    @Test
+    public void mcdclevel0() {
+    	assertEquals(2.0, (App.level0(new Person(false, 22, false, true, false))), 0.0);
+    	assertEquals(5.0, (App.level0(new Person(false, 24, false, true, false))), 0.0);
+    	assertEquals(1.0, (App.level0(new Person(false, 66, false, true, false))), 0.0);
+    	assertEquals(5.0, (App.level0(new Person(false, 64, false, true, false))), 0.0);
+    }
+    @Test
+    public void mcdclevel1() {
+    	assertEquals(3.5, (App.level1(new Person(false, 22, false, true, false))), 0.0);
+    	assertEquals(5.0, (App.level1(new Person(false, 24, false, true, false))), 0.0);
+    	assertEquals(2.5, (App.level1(new Person(false, 66, false, true, false))), 0.0);
+    	assertEquals(5.0, (App.level1(new Person(false, 64, false, true, false))), 0.0);
+    }
+    @Test
+    public void mcdclevel2() {
+    	assertEquals(6.0, (App.level2(new Person(false, 66, false, true, false))), 0.0);
+    	assertEquals(5.0, (App.level2(new Person(false, 64, false, true, false))), 0.0);
+    }
+    @Test
+    public void mcdclevel3() {
+    	assertEquals(6.0, (App.level3(new Person(false, 22, false, true, false))), 0.0);
+    	assertEquals(5.0, (App.level3(new Person(false, 24, false, true, false))), 0.0);
+    	assertEquals(7.5, (App.level3(new Person(false, 66, false, true, false))), 0.0);
+    	assertEquals(5.0, (App.level3(new Person(false, 64, false, true, false))), 0.0);
+    }
+    @Test
+    public void mcdclevel4() {
+    	assertEquals(7.5, (App.level4(new Person(false, 22, false, true, false))), 0.0);
+    	assertEquals(5.0, (App.level4(new Person(false, 24, false, true, false))), 0.0);
+    }
+    @Test
+    public void mcdcValid() {
+    	assertEquals(true, (App.valid(new Person(false, 66, false,true, false), 499)));
+    	assertEquals(false, (App.valid(new Person(true, 66, true, false, true), 501)));
+    }
+    @Test
+    public void mcdcRun() {
+    	try {
+			assertEquals(5.0, (App.run(new Person(false, 38, false, true, false), 99)), 0.0);
+			assertEquals(NotAllowedException.class, (App.run(new Person(true, 38, true, false, true), 101)));
+			assertEquals(5.0, (App.run(new Person(false, 38, false, true, false), 199)), 0.0);
+			assertEquals(NotAllowedException.class, (App.run(new Person(true, 38, true, false, true), 201)));
+			assertEquals(5.0, (App.run(new Person(false, 38, false, true, false), 300)), 0.0);
+			assertEquals(NotAllowedException.class, (App.run(new Person(true, 38, true, false, true), 301)));
+			assertEquals(5.0, (App.run(new Person(false, 38, false, true, false), 500)), 0.0);
+			assertEquals(NotAllowedException.class, (App.run(new Person(true, 38, true, false, true), 501)));
+			assertEquals(5.0, (App.run(new Person(false, 38, false, true, false), 501)), 0.0);
+			assertEquals(NotAllowedException.class, (App.run(new Person(true, 38, true, false, true), 499)));
+    	} catch (NotAllowedException e) {
+			
+		}
     }
 }
